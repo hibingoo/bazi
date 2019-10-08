07538fdcbb782b830f4c592e8d01bb13d85e8215
hibingoo <33690138+hibingoo@users.noreply.github.com>
2019-10-08 08:40:21 +0800

    Update index.vue

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,56 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<view class="uni-padding-wrap">
+			<form @submit="formSubmit" @reset="formReset">
+				<view>
+					<view class="uni-title">姓名</view>
+					<view class="uni-list">
+						<view class="uni-list-cell">
+							<input class="uni-input" name="nickname" placeholder="请填写您的昵称" />
+						</view>
+					</view>
+				</view>
+				<view>
+					<view class="uni-title">性别</view>
+					<radio-group class="uni-flex" name="gender">
+						<label>
+							<radio value="男" />男</label>
+						<label>
+							<radio value="女" />女</label>
+					</radio-group>
+				</view>
+
+				<view class="uni-list">
+					<view class="uni-list-cell">
+						<view class="uni-list-cell-left">
+							日期选择
+						</view>
+						<view class="uni-list-cell-db">
+							<picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
+								<view class="uni-input">{{date}}</view>
+							</picker>
+						</view>
+					</view>
+				</view>
+
+				<view class="uni-list">
+					<view class="uni-list-cell">
+						<view class="uni-list-cell-left">
+							时间选择
+						</view>
+						<view class="uni-list-cell-db">
+							<picker mode="time" :value="time" start="09:01" end="21:01" @change="bindTimeChange">
+								<view class="uni-input">{{time}}</view>
+							</picker>
+						</view>
+					</view>
+				</view>
+
+				<view class="uni-btn-v uni-common-mt">
+					<button class="btn-submit" formType="submit" type="primary">Submit</button>
+					<button type="default" formType="reset">Reset</button>
+				</view>
+			</form>
 		</view>
 	</view>
 </template>
